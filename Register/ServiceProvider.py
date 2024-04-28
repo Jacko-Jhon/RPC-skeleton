5e47b0918dc04ece911c236a9f8666d2a042ca81
Jacko <1730072668@qq.com>
2024-04-28 21:06:46 +0800

    继续优化

--- Register/ServiceProvider.py
+++ Register/ServiceProvider.py
@@ -8,8 +8,5 @@ class ServiceProvider(Register):
         services_list = []
         if name not in Register._name_to_id: return services_list
         services_id = Register._name_to_id[name]
-        for id in services_id:
-            if Register._services[id].status >= 1:
-                services_list.append(id)
         services_list = [Register._services[id] for id in services_id if Register._services[id].status >= 1]
         return services_list
